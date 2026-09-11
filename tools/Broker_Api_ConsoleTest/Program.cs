726e82136da33ce8df12a1d37f31809edae2aeff
MajkRitcherd <misutak87@gmail.com>
2026-09-11 10:58:03 +0200

    feat: Read configuration from appsettings.json

--- tools/Broker_Api_ConsoleTest/Program.cs
+++ tools/Broker_Api_ConsoleTest/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using BrokerApiConsoleTest.Trading212;
+
+var configuration = new ConfigurationT212("appsettings.json");
+System.Console.WriteLine(configuration);
---
+++ tools/Broker_Api_ConsoleTest/Trading212/Configuration.cs
@@ -0,0 +1,39 @@
+using Microsoft.Extensions.Configuration;
+
+namespace BrokerApiConsoleTest.Trading212
+{
+    /// <summary>
+    /// Holds configuration to Trading 212 API, such as BaseUrl, ApiKey and ApiSecret.
+    /// </summary>
+    /// <param name="pathToJsonFile">Path to AppSettings.json file.</param>
+    public class ConfigurationT212(string pathToJsonFile)
+    {
+        private const string configurationKey = "Trading212";
+
+        private readonly IConfigurationRoot configurationBuilder = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile(pathToJsonFile, optional: true, reloadOnChange: true)
+            .Build();
+
+        /// <summary>
+        /// Gets Trading 212 API key ('username').
+        /// </summary>
+        public string? ApiKey => configurationBuilder[$"{configurationKey}:ApiKey"];
+
+        /// <summary>
+        /// Gets Trading 212 API Secret ('password').
+        /// </summary>
+        public string? ApiSecret => configurationBuilder[$"{configurationKey}:ApiSecret"];
+
+        /// <summary>
+        /// Gets base URL to Trading 212 API.
+        /// </summary>
+        public string? BaseUrl => configurationBuilder[$"{configurationKey}:BaseUrl"];
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return $"{configurationKey} configuration\nBaseUrl: {BaseUrl}\nApiKey: {ApiKey}\nApiSecret: {ApiSecret}";
+        }
+    }
+}
